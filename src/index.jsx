08f44e6aed7e9c86cb6d14813b120cbcf0c1377e
saiani1 <saiani@naver.com>
2022-12-24 20:50:14 +0900

    Update: 프로젝트 세팅

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
+import App from './App';
+import './styles/index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
