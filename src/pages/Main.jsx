52d286add204ce2348551b22e2b40b06e97e9cb2
Im-Hass <huiseon4107@gmail.com>
2023-01-05 23:43:06 +0900

    Refactor: 가게정보 컴포넌트로 분리

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import StoreImg from '../components/content/StoreImg';
-import imgUrl from '../assets/img/1000x1000.png';
 
 import styles from './main.module.scss';
-import Title from '../components/common/Title';
+import StoreInfo from '../components/content/StoreInfo';
 
 const cx = classNames.bind(styles);
 
 function Main() {
   return (
     <div className={cx('main-wrap')}>
-      <Title txt='가게정보' />
-
-      <div className={cx('imgs-wrap')}>
-        <StoreImg url={imgUrl} alt='가게외부사진1' desc='가게 외부1' />
-        <StoreImg url={imgUrl} alt='가게외부사진2' desc='가게 외부2' />
-        <StoreImg url={imgUrl} alt='가게외부사진3' desc='가게 내부1' />
-        <StoreImg url={imgUrl} alt='가게외부사진3' desc='가게 내부2' />
-      </div>
+      <StoreInfo />
     </div>
   );
 }
