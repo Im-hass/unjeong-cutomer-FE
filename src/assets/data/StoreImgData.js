389d74bf81fed5b158df6cf750256d6bf54d074a
Im-Hass <huiseon4107@gmail.com>
2023-01-12 23:23:19 +0900

    Update: 가게 image 추가

---
+++ src/assets/data/StoreImgData.js
@@ -0,0 +1,20 @@
+export const STORE_IMG_LIST = [
+  {
+    id: 1,
+    url: 'http://121.145.206.143:12230/api/static/img/store_1',
+    alt: '가게외부사진1',
+    desc: '가게 외부',
+  },
+  {
+    id: 2,
+    url: 'http://121.145.206.143:12230/api/static/img/store_2',
+    alt: '가게외부사진2',
+    desc: '가게 외부',
+  },
+  {
+    id: 3,
+    url: 'http://121.145.206.143:12230/api/static/img/store_3',
+    alt: '가게외부사진3',
+    desc: '가게 내부',
+  },
+];
